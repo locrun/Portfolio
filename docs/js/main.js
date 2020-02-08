eae242bcc42ce4f8a0915153ea26bfb548f0de76
Runar <github.runar@gmail.com>
2020-02-08 22:52:35 +0300

    test

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
 
-
   //slide2id - плавная прокрутка по ссылкам внутри страницы
   $("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
     highlightSelector: "nav a"
