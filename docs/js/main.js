104681354fb133e52183b28492d0b84f34344792
Runar <github.runar@gmail.com>
2020-11-29 18:35:11 +0300

    commit:editing resume

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,32 +1,28 @@
-$(document).ready(function() {
-
-
+$(document).ready(function () {
   //slide2id - плавная прокрутка по ссылкам внутри страницы
-  $("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
-    highlightSelector: "nav a"
-
+  $(
+    "nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id"
+  ).mPageScroll2id({
+    highlightSelector: "nav a",
   });
 
   // $("#resume").on("click", "a"function(event) {
   //     event.preventDefault();
   //   }
 
-
   // MixItUp - фильтрация работ в портфолио
-  $('#portfolio-projects').mixItUp();
-
+  $("#portfolio-projects").mixItUp();
 
   // FancyBox - galery
   $(".fancybox").fancybox({
     // Default - with fix from scroll to top
     helpers: {
       overlay: {
-        locked: false
-      }
-    }
+        locked: false,
+      },
+    },
   });
   // End of FancyBox - galery
-
 
   // jQuery Validate JS
   $("#contact-form").validate({
@@ -35,28 +31,27 @@ $(document).ready(function() {
       email: { required: true, email: true },
       // skype:  { required: true },
       // phone:  { required: true },
-      message: { required: true }
+      message: { required: true },
     },
 
     messages: {
       name: "Пожалуйста, введите свое имя",
       email: {
         required: "Пожалуйста, введите свой email",
-        email: "Email адрес должен быть в формате name@domain.com . Возможно вы ввели email с ошибкой."
+        email:
+          "Email адрес должен быть в формате name@domain.com . Возможно вы ввели email с ошибкой.",
       },
-      message: "Пожалуйста, введите текст сообщения"
+      message: "Пожалуйста, введите текст сообщения",
     },
 
-
-    submitHandler: function(form) {
+    submitHandler: function (form) {
       ajaxFormSubmit();
-    }
-  })
-
+    },
+  });
 
   // Функция AJAX запрса на сервер
   function ajaxFormSubmit() {
-    var string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку. 
+    var string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку.
 
     // Формируем ajax запрос
     $.ajax({
@@ -65,14 +60,13 @@ $(document).ready(function() {
       data: string, // Какие даные отправляем, в данном случае отправляем переменную string
 
       // Функция если все прошло успешно
-      success: function(html) {
+      success: function (html) {
         $("#contact-form").slideUp(800);
-        $('#answer').html(html);
-      }
+        $("#answer").html(html);
+      },
     });
 
     // Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
     return false;
   }
-
 });
